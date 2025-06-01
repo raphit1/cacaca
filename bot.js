98c2c39eba1efad9e288672d6a194cc265b928f3
raphit1 <raphchaman1@gmail.com>
2025-06-01 04:25:38 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,34 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+const TOKEN = process.env.TOKEN;
+
+const pooWords = ['merde', 'chier', 'caca'];
+const eggplantWords = ['pipi', 'bite', 'queue', 'paf', 'paff'];
+
+client.once('ready', () => {
+  console.log(`Bot connecté en tant que ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  const content = message.content.toLowerCase();
+
+  if (pooWords.some(word => content.includes(word))) {
+    message.react('💩').catch(console.error);
+  }
+
+  if (eggplantWords.some(word => content.includes(word))) {
+    message.react('🍆').catch(console.error);
+  }
+});
+
+client.login(TOKEN);
